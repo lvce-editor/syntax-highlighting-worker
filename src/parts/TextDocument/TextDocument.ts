8565ce15257f8f8f25371f054eaf5c0e158e0c9c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-07-28 19:41:59 +0200

    feature: add back syntax highlighting caching (#4)

--- src/parts/TextDocument/TextDocument.ts
+++ src/parts/TextDocument/TextDocument.ts
@@ -1,9 +1,37 @@
 const lineMap = Object.create(null)
+const lineCaches = Object.create(null)
+const invalidStartIndices = Object.create(null)
+const languageIds = Object.create(null)
 
 export const setLines = (editorId: number, lines: readonly string[]) => {
   lineMap[editorId] = lines
+  lineCaches[editorId] = []
+  invalidStartIndices[editorId] = 0
 }
 
 export const getLines = (editorId: number) => {
   return lineMap[editorId]
+}
+
+export const getLineCache = (editorId: number) => {
+  return lineCaches[editorId]
+}
+
+export const getInvalidStartIndex = (editorId: number) => {
+  return invalidStartIndices[editorId]
+}
+
+export const resetInvalidStartIndex = (languageId: string) => {
+  for (const [key, value] of Object.entries(languageIds)) {
+    if (value === languageId) {
+      invalidStartIndices[key] = 0
+    }
+  }
+}
+export const setInvalidStartIndex = (editorId: number, index: number) => {
+  invalidStartIndices[editorId] = index
+}
+
+export const setLanguageId = (editorId: number, languageId: string) => {
+  languageIds[editorId] = languageId
 }
--- src/parts/TokenizerMap/TokenizerMap.ts
+++ src/parts/TokenizerMap/TokenizerMap.ts
@@ -1,9 +1,11 @@
 import * as TokenizePlainText from '../TokenizePlainText/TokenizePlainText.ts'
+import * as TextDocument from '../TextDocument/TextDocument.ts'
 
 const tokenizers = Object.create(null)
 
 export const set = (id, value) => {
   tokenizers[id] = value
+  TextDocument.resetInvalidStartIndex(id)
 }
 
 export const get = (id) => {
