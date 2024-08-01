67562e38f219c391be911aaf8979049bf70769ec
Le Vivilet <levivlet@pm.me>
2024-08-01 17:45:42 +0200

    feature: add function to tokenize a single line incrementally

--- src/parts/CommandMap/CommandMap.ts
+++ src/parts/CommandMap/CommandMap.ts
@@ -3,12 +3,14 @@ import * as GetTokensViewport from '../GetTokensViewport/GetTokensViewport.ts'
 import * as HandleMessagePort from '../HandleMessagePort/HandleMessagePort.ts'
 import * as TextDocument from '../TextDocument/TextDocument.ts'
 import * as TokenizeCodeBlock from '../TokenizeCodeBlock/TokenizeCodeBlock.ts'
+import * as TokenizeIncremental from '../TokenizeIncremental/TokenizeIncremental.ts'
 import * as Tokenizer from '../Tokenizer/Tokenizer.ts'
 
 export const commandMap = {
-  [CommandId.TokenizeCodeBlock]: TokenizeCodeBlock.tokenizeCodeBlock,
-  [CommandId.LoadTokenizer]: Tokenizer.loadTokenizer,
-  [CommandId.HandleMessagePort]: HandleMessagePort.handleMessagePort,
   [CommandId.GetTokensViewport]: GetTokensViewport.getTokensViewport,
+  [CommandId.HandleMessagePort]: HandleMessagePort.handleMessagePort,
+  [CommandId.LoadTokenizer]: Tokenizer.loadTokenizer,
   [CommandId.SetLines]: TextDocument.setLines,
+  [CommandId.TokenizeCodeBlock]: TokenizeCodeBlock.tokenizeCodeBlock,
+  [CommandId.TokenizeIncremental]: TokenizeIncremental.tokenizeIncremental,
 }
