040bd912534c9066476c95ce1824505f9f79395e
Le Vivilet <levivlet@pm.me>
2025-01-12 18:18:59 +0100

    fix: ipc ready message being sent twice

--- src/parts/HandleMessagePort/HandleMessagePort.ts
+++ src/parts/HandleMessagePort/HandleMessagePort.ts
@@ -1,11 +1,9 @@
 import { MessagePortRpcClient } from '@lvce-editor/rpc'
 import * as DefaultCommandMap from '../DefaultCommandMap/DefaultCommandMap.ts'
 
-export const handleMessagePort = async (port: MessagePort) => {
+export const handleMessagePort = async (port: MessagePort): Promise<void> => {
   await MessagePortRpcClient.create({
     commandMap: DefaultCommandMap.commandMap,
     messagePort: port,
   })
-  // TODO this should be handled by rpc
-  port.postMessage('ready')
 }
