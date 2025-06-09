9f0d7a0add4a01d93758d0443944ffe6b91bfe5f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-09 12:26:30 +0200

    feature: add function to create simpler rpc (#37)

--- src/parts/HandleMessagePort/HandleMessagePort.ts
+++ src/parts/HandleMessagePort/HandleMessagePort.ts
@@ -1,8 +1,15 @@
-import { MessagePortRpcClient } from '@lvce-editor/rpc'
+import { MessagePortRpcClient, PlainMessagePortRpc } from '@lvce-editor/rpc'
 import * as DefaultCommandMap from '../DefaultCommandMap/DefaultCommandMap.ts'
 
 export const handleMessagePort = async (port: MessagePort): Promise<void> => {
   await MessagePortRpcClient.create({
+    commandMap: DefaultCommandMap.commandMap,
+    messagePort: port,
+  })
+}
+
+export const handleMessagePort2 = async (port: MessagePort): Promise<void> => {
+  await PlainMessagePortRpc.create({
     commandMap: DefaultCommandMap.commandMap,
     messagePort: port,
   })
